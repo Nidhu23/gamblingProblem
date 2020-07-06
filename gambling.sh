7b13924924307fb25883d38dfd4babc9f3cdc064
“Nidhu23” <nidhunanik@gmail.com>
2020-07-06 16:02:20 +0530

    [Nidhuna]Add: Code to check bet won or lost

--- gambling.sh
+++ gambling.sh
@@ -1,8 +1,19 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome To Gambling Simulator"
 
 stake=100;
 bet=1;
+
+result=$(($RANDOM%2))
+
+if [[ $result -eq 1 ]]
+then
+	echo "You won 1 dollar"
+	total=$(($stake+1))
+else
+	echo "You lost 1 dollar"
+	total=$(($stake-1))
+fi
 
 
