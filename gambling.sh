43635fbc012f609b8cad8a270443d7496b8a297d
“Nidhu23” <nidhunanik@gmail.com>
2020-07-06 16:30:53 +0530

    [Nidhuna]Add: Code to check for resign condition

--- gambling.sh
+++ gambling.sh
@@ -4,16 +4,21 @@ echo "Welcome To Gambling Simulator"
 
 stake=100;
 bet=1;
+maxWin=150;
+maxLose=50;
 
-result=$(($RANDOM%2))
+total=$((stake));
 
-if [[ $result -eq 1 ]]
-then
-	echo "You won 1 dollar"
-	total=$(($stake+1))
-else
-	echo "You lost 1 dollar"
-	total=$(($stake-1))
-fi
-
+while [[ $total -le $maxWin && $total -ge $maxLose ]]
+do
+	result=$(($RANDOM%2))
+	if [[ $result -eq 1 ]]
+	then
+		echo "You won 1 dollar"
+		((total++))
+	else
+		echo "You lost 1 dollar"
+		((total--))
+	fi
+done
 
