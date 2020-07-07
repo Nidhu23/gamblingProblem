5bcd989c1f7290a56f351ebe12acbe144a1a8a35
“Nidhu23” <nidhunanik@gmail.com>
2020-07-07 16:48:14 +0530

    [Nidhuna]Add: Compute days won or lost and amount

--- gambling.sh
+++ gambling.sh
@@ -4,56 +4,59 @@ echo "Welcome To Gambling Simulator"
 
 declare -A dailyAmount
 
-STAKE=100;
+INITIAL_STAKE=100;
 BET=1;
-STAKE_PERCENTAGE=$(($((STAKE/100))*50))
-MAX_WIN=$((STAKE+STAKE_PERCENTAGE));
-MAX_LOSE=$((STAKE-STAKE_PERCENTAGE));
-MAX_DAYS=20;
-TOTAL_BET_AMT=$((STAKE*MAX_DAYS))
+MAX_DAYS=28;
 
-totalPerDay=$((STAKE));
+totalPerDay=$((INITIAL_STAKE));
 finalAmt=0;
+newStake=0;
+stakeAmt=0;
+
+newStakePercent(){
+    stakePercentage=$(($newStake/2))
+    maxWin=$((newStake+stakePercentage));
+    maxLose=$((newStake-stakePercentage));
+}
 
 dailyCalculation(){
-	while [[ $totalPerDay -lt $MAX_WIN && $totalPerDay -gt $MAX_LOSE ]]
-	do
-		result=$(($RANDOM%2))
-		if [[ $result -eq 1 ]]
-		then
-			((totalPerDay++))
-		else
-			((totalPerDay--))
-		fi
-	done
+    while [[ $totalPerDay -lt $maxWin && $totalPerDay -gt $maxLose ]]
+    do
+        result=$(($RANDOM%2))
+        if [[ $result -eq 1 ]]
+        then
+            ((totalPerDay++))
+        else
+            ((totalPerDay--))
+        fi
+    done
 }
 
 totalAmount(){
-	for (( day=1; day<=$MAX_DAYS; day++ ))
-	do
-		dailyCalculation
-		dailyAmount[$day]=$((totalPerDay))
-		finalAmt=$(($finalAmt+$totalPerDay))
-		totalPerDay=$((STAKE))
-	done
+    for (( day=1; day<=$MAX_DAYS; day++ ))
+    do
+    newStake=$(($stakeAmt+$INITIAL_STAKE))
+    newStakePercent
+    dailyCalculation
+    wonOrLost $day
+    dailyAmount[$day]=$(($totalPerDay))
+    stakeAmt=$(($totalPerDay))
+    done
 }
 
 printDailyAmt(){
-	for ((day=1;day<=$MAX_DAYS;day++))
-	do
-		echo -e "Final Amount On Day" $day "\t" ${dailyAmount[$day]} "\n"
-	done
+    for ((day=1;day<=$MAX_DAYS;day++))
+    do
+        echo -e "Final Amount On Day" $day "\t" ${dailyAmount[$day]} "\n"
+    done
 }
 
-winOrLose(){
-	if [[ $finalAmt -gt $TOTAL_BET_AMT ]]
-	then
-		echo "AT THE END OF 20 DAYS, YOU WON $"$(($finalAmt-$TOTAL_BET_AMT))
-	else
-		echo "AT THE END OF 20 DAYS, YOU LOST $"$(($TOTAL_BET_AMT-$finalAmt))
-	fi
+wonOrLost(){
+    if [[ $newStake -lt $totalPerDay ]]
+    then
+        echo "Start: $newStake You won on Day"$1 $(($totalPerDay-$newStake)) "End: $totalPerDay"
+    else
+        echo "Start: $newStake You lost on Day"$1 $(($newStake-$totalPerDay)) "End: $totalPerDay"
+    fi
 }
-
 totalAmount
-printDailyAmt
-winOrLose
