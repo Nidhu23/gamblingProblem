0c1df65f542f0fca122fa9c855e234f5b0998303
“Nidhu23” <nidhunanik@gmail.com>
2020-07-08 10:31:49 +0530

    [Nidhuna]Refactor: Rectified a test case error

--- gambling.sh
+++ gambling.sh
@@ -2,93 +2,106 @@
 
 echo "Welcome To Gambling Simulator"
 
-declare -a lucky
-declare -a unlucky
 
 INITIAL_STAKE=100;
 BET=1;
-MAX_DAYS=28;
+MAX_DAYS=10;
 
-totalPerDay=$((INITIAL_STAKE));
-finalAmt=0;
-newStake=0;
-stakeAmt=0;
-tempWin=0;
-tempLost=0;
-dayWin=0;
-dayLose=0;
-winTotal=0;
-loseTotal=0;
-month=1;
+totalPerDay=0
+winTemp=0
+loseTemp=0
+winDay=0
+loseDay=0
+winTotal=0
+loseTotal=0
+month=1
 
-newStakePercent(){
-        stakePercentage=$(($newStake/2))
-        maxWin=$((newStake+stakePercentage));
-        maxLose=$((newStake-stakePercentage));
+unluckyCheck(){
+	if [[ $loseTemp -lt $newStake ]]
+	then
+		loseTemp=$(($newStake))
+		loseDay=$(($day))
+	fi
+}
+
+luckyCheck(){
+	if [[ $winTemp -lt $newStake ]]
+	then
+		winTemp=$(($newStake))
+		winDay=$(($day))
+	fi
 }
 
 dailyCalculation(){
-    while [[ $totalPerDay -lt $maxWin && $totalPerDay -gt $maxLose ]]
+	while [[ $totalPerDay -lt $maxWin && $totalPerDay -gt $maxLose ]]
     do
-        result=$(($RANDOM%2))
-        if [[ $result -eq 1 ]]
+        if [[ $(($RANDOM%2)) -eq 0 ]]
         then
-            ((totalPerDay++))
+            totalPerDay=$(($totalPerDay-$BET))
         else
-            ((totalPerDay--))
+            totalPerDay=$(($totalPerDay+$BET))
         fi
     done
-}
-
-totalAmount(){
-    for (( day=1; day<=$MAX_DAYS; day++ ))
-    do
-        newStake=$(($stakeAmt+$INITIAL_STAKE))
-        newStakePercent
-        dailyCalculation
-        wonOrLost $day
-  		stakeAmt=$(($totalPerDay))
-    done
-}
-
-wonOrLost(){
-    if [[ $newStake -lt $totalPerDay ]]
+    if [[ $totalPerDay -gt $newStake ]]
     then
-		winTotal=$(($winTotal+$totalPerDay))
-        result=$(($totalPerDay-$newStake))
-        echo "Start: $newStake You won on Day"$1 $result "End: $totalPerDay"
-        if [[ $result -gt $tempWin ]]
+        if [[ $(($totalPerDay-$newStake)) -eq 1 ]]
         then
-            tempWin=$(($result))
-            dayWin=$(($1))
+			unluckyCheck
+            loseTotal=$(($loseTotal+$newStake))
+            echo "At the End of day $day: $totalPerDay Lost $newStake"
+        else
+			luckyCheck
+            winTotal=$(($winTotal+$newStake))
+            echo "At the End of day $day: $totalPerDay won $newStake"
         fi
     else
-		loseTotal=$(($loseTotal+$totalPerDay))
-        result2=$(($newStake-$totalPerDay))
-        echo "Start: $newStake You lost on Day"$1 $result2 "End: $totalPerDay"
-        if [[ $result2 -gt $tempLose ]]
-        then
-            tempLose=$(($result2))
-            dayLose=$(($1))
-        fi
-
+		unluckyCheck
+        loseTotal=$(($loseTotal+$newStake))
+        echo "At the End of day $day: $totalPerDay Lost $newStake"
     fi
 }
 
-echo "For month 1: "
-totalAmount
-echo -e "Luckiest Day " $dayWin "\nUnluckiest Day: " $dayLose
+totalAmount(){
+	totalPerDay=$(($totalPerDay+$INITIAL_STAKE))
+    newStake=$(($totalPerDay/2))
+    maxWin=$(($totalPerDay+$newStake))
+    maxLose=$(($totalPerDay-$newStake))
+	dailyCalculation
+}
 
-while (( $month <=12 ))
-do
-	if [[ winTotal -gt loseTotal ]]
-	then
-		((++month))
-		echo "For $month:"
+play(){
+	for (( day=1; day<=MAX_DAYS; day++ ))
+	do
 		totalAmount
-		echo -e "Luckiest Day " $dayWin "\nUnluckiest Day: " $dayLose
-	else
-		echo "You faced too much loss in month $month"
-		exit
-	fi
-done
+	done
+}
+
+print(){
+	 echo "Your unluckiest day is $loseDay, You lost $loseTemp"
+     echo "Your luckiest day is $winDay, You won $winTemp"
+}
+
+gameContinue(){
+	while [[ $month -le 12 ]]
+	do
+    	if [[ $month -eq 1 ]]
+    	then
+        	echo "For month $month"
+        	play
+			print
+    	else
+        	if [[ $winTotal -gt $loseTotal ]]
+        	then
+            	echo "For month $month"
+            	play
+				print
+        	else
+				echo "You faced too much loss in month $(($month-1)), you lost $loseTotal"
+            	exit
+        	fi
+    	fi
+    	((month++))
+	done
+}
+
+gameContinue
